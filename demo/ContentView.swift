010795fd63721153cf6ac9b50f21b38ef4b71f50
trisha-ravi <trisha02.r@gmail.com>
2024-09-10 15:57:55 -0400

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by Trisha Ravichandran on 9/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Trisha Ravichandran on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
